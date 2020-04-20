45227150d798f158082ea3e924b709b6f8989612
Oleksandr Bessonov <oleksandr.bessonov@roh.org.uk>
2020-04-20 20:21:31 +0300

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="sadas"/>
   </div>
 </template>
 
