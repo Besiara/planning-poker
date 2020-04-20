291b4992cfdb77ba26a2f4de3dbd769dae0a721d
Besiara <Besiara@users.noreply.github.com>
2020-04-20 16:49:19 +0000

    deploy: 868c00679a447a0b45be7bff86fe54a55c8517be

---
+++ precache-manifest.355c3d62361bc9871793a167488f0a0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac100d41ea1773694467",
+    "url": "/css/app.80a22fda.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bc099305e57ff98c9d92a55a2f84f10a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a673cf49b73a4958a4",
+    "url": "/js/about.cd89591a.js"
+  },
+  {
+    "revision": "ac100d41ea1773694467",
+    "url": "/js/app.1d5299aa.js"
+  },
+  {
+    "revision": "45e13053ec1f58887475",
+    "url": "/js/chunk-vendors.ade13aea.js"
+  },
+  {
+    "revision": "56488f5675ba887d72c91a11efc708e5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
